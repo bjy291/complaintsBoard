cb5ea79569e75b2d5c79218ba9984c34c1cca592
bjy291 <qor291@gmail.com>
2021-11-23 01:41:24 +0900

    차트 추가

--- route/controller/rootctr.js
+++ route/controller/rootctr.js
@@ -4,4 +4,5 @@ const root=require('../model/root');
 router.get('/', root.root)
 router.post('/', root.rootpost)
 
+router.get('/chart1', root.chart1)
 module.exports = router
--- route/model/root.js
+++ route/model/root.js
@@ -19,3 +19,38 @@ exports.rootpost=function(req,res){
                 }
         })
 }
+
+exports.chart1=function(req,res){ 
+        var date = new Date();
+        var month = date.getMonth()+1
+        var sql = "SELECT date_format(mb_date,'%m') as month, date_format(mb_date,'%d') as day FROM min_board WHERE mb_date BETWEEN '2021-?-01' AND '2021-?-30';"
+
+        db.query(sql,[month, month],function(err, result){
+                if(err) console.log(err)
+                else{
+                        sql = "SELECT  date_format(mb_date,'%d') as day, COUNT(*) as cnt FROM min_board WHERE mb_date BETWEEN '2021-?-01' AND '2021-?-30' GROUP BY 1"
+                        db.query(sql,[month, month], function(err, result2){
+                                if(err) console.log("chart sql 2 err : " + err);
+                                else{
+                                        if(req.session.displayname){
+                                                var dname = req.session.displayname;  
+                                                var DayList = [];
+                                                for (day of result2){
+                                                        DayList.push(day.day);
+                                                }
+
+                                                var CntList = [];
+                                                for (cnt of result2){
+                                                        CntList.push(cnt.cnt)
+                                                }
+                                                console.log(DayList[0]);
+                                                res.render('chart1', {name:dname, id:req.session.user, chartDAY: DayList, chartCNT:CntList});
+                                        }else{
+                                                res.render('chart1');
+                                        }   
+                                }
+                        }) 
+                        
+                }
+        })
+    };
