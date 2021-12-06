7bd9f3a9e298ab3d0bf12ce09ae6ceedba07259e
bjy291 <qor291@gmail.com>
2021-12-07 02:09:54 +0900

    짜잘한 부분 수정

--- app.js
+++ app.js
@@ -33,7 +33,7 @@
 //     app.use(bodyParser.urlencoded({extended:true}));
 //     app.use(bodyParser.json());
     app.use(express.json({ limit : "50mb" })); 
-    app.use(express.urlencoded({ limit:"50mb", extended: false }));
+    app.use(express.urlencoded({ limit:"50mb", extended: true }));
     app.use(session({
             secret:'test',
             resave:false,
